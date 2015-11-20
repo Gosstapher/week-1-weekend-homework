522e51ae104b0e5e87651e1519eb159b25a7c16c
Gosstapher <gosstapher@gmail.com>
2015-11-20 12:50:41 +0000

    scrapped previous array, added full WAREHOUSE array.

--- warehouse_functions.rb
+++ warehouse_functions.rb
@@ -1,33 +1,169 @@
-warehouse = [
+WAREHOUSE = [
   {
-    :a1 => "needle",
-    :a2 => "stop sign",
-    :a3 => "blouse",
-    :a4 => "hanger",
-    :a5 => "rubber duck",
-    :a6 => "shovel",
-    :a7 => "bookmark",
-    :a8 => "model car",
-    :a9 => "glow stick",
-    :a10 => "rubber band"
-  }, 
-  {
-    :b1 => "tyre swing",
-    :b2 => "sharpie",
-    :b3 => "picture frame",
-    :b4 => "photo album",
-    :b5 => "nail filer"
-
+    :letter => "a",
+    :number => "1",
+    :content => "needle"
   },
-  {:c1 => "rusty nail"
-  }
+  {
+    :letter => "a",
+    :number => "2",
+    :content => "stop sign"
+  },
+  {
+    :letter => "a",
+    :number => "3",
+    :content => "blouse"
+  },
+  {
+    :letter => "a",
+    :number => "4",
+    :content => "hanger"
+  },
+  {
+    :letter => "a",
+    :number => "5",
+    :content => "rubber duck"
+  },
+  {
+    :letter => "a",
+    :number => "6",
+    :content => "shovel"
+  },
+  {
+    :letter => "a",
+    :number => "7",
+    :content => "bookmark"
+  },
+  {
+    :letter => "a",
+    :number => "8",
+    :content => "model car"
+  },
+  {
+    :letter => "a",
+    :number => "9",
+    :content => "glowstick"
+  },
+  {
+    :letter => "a",
+    :number => "10",
+    :content => "rubber band"
+  },
+  {
+    :letter => "b",
+    :number => "1",
+    :content => "tyre swing"
+  },
+  {
+    :letter => "b",
+    :number => "2",
+    :content => "sharpie"
+  },
+  {
+    :letter => "b",
+    :number => "3",
+    :content => "picture frame"
+  },
+  {
+    :letter => "b",
+    :number => "4",
+    :content => "photo album"
+  },
+  {
+    :letter => "b",
+    :number => "5",
+    :content => "nail filer"
+  },
+  {
+    :letter => "b",
+    :number => "6",
+    :content => "tooth paste"
+  },
+  {
+    :letter => "b",
+    :number => "7",
+    :content => "bath fizzers"
+  },
+  {
+    :letter => "b",
+    :number => "8",
+    :content => "tissue box"
+  },
+  {
+    :letter => "b",
+    :number => "9",
+    :content => "deodorant"
+  },
+  {
+    :letter => "b",
+    :number => "10",
+    :content => "cookie jar"
+  },
+  {
+    :letter => "c",
+    :number => "1",
+    :content => "rusty nail"
+  },
+  {
+    :letter => "c",
+    :number => "2",
+    :content => "drill press"
+  },
+  {
+    :letter => "c",
+    :number => "3",
+    :content => "chalk"
+  },
+  {
+    :letter => "c",
+    :number => "4",
+    :content => "word search"
+  },
+  {
+    :letter => "c",
+    :number => "5",
+    :content => "thermometer"
+  },
+  {
+    :letter => "c",
+    :number => "6",
+    :content => "face wash"
+  },
+  {
+    :letter => "c",
+    :number => "7",
+    :content => "paint brush"
+  },
+  {
+    :letter => "c",
+    :number => "8",
+    :content => "candy wrapper"
+  },
+  {
+    :letter => "c",
+    :number => "9",
+    :content => "shoe lace"
+  },
+  {
+    :letter => "c",
+    :number => "10",
+    :content => "leg warmers"
+  },
 ]
 
 
 
 
 def item_at_bay()
-  for i in warehouse
-    puts i[:b5]
-  end
+  return WAREHOUSE[1][:b5]
 end
+
+
+
+
+
+
+
+
+
+
--- warehouse_runner.rb
+++ warehouse_runner.rb
@@ -2,4 +2,4 @@ require_relative ('./warehouse_functions')
 
 # Given a bay, returns the item in that bay
 found_item = item_at_bay()
-puts "item at b5 found is #{found_item}, expect it to be nail filer"
+puts "item found at b5 is #{found_item}, expect it to be nail filer"
